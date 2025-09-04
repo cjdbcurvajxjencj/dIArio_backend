55d88498111a6e852234b18169c9bbe118e7284e
cjdbcurvajxjencj <cjdbcurvajxjencj@gmail.com>
2025-09-04 14:26:55 +0200

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-# 1. Installa le librerie Python
-pip install -r requirements.txt
-
-# 2. Installa ffmpeg
-apt-get update && apt-get install -y ffmpeg
